821d6d3b4457137c7c5997520a90d8e0644b1a31
munja-cpu <munjasjovan@gmail.com>
2021-10-02 03:06:12 +0200

    dodata forma i php

--- contactform.php
+++ contactform.php
@@ -10,6 +10,6 @@ if (isset($_POST['submit'])){
     $txt = "You have received an e-mail from".$name.".\n\n".$message;
 
     mail($mailTo, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 
 }
