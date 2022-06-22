a90e261f6dd409e195d9d6db96ebb4d62d12d4a4
munja-cpu <munjasjovan@gmail.com>
2022-06-22 22:25:39 +0200

    dodat gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-var gulp = require('gulp');
-var uglifycss = require('gulp-uglifycss');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const uglifyCss = require('gulp-uglifycss');
 
-gulp.task('css', async function(){
-    gulp.src('./style.css')
-    .pipe(uglifycss({
-        "uglyComments": true
-    }))
-    .pipe(gulp.dest('./dist/'));
-    
+
+// Minify JS
+gulp.task('minify', function(){
+    gulp.src('main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
+// Minify CSS
+gulp.task('minifyCss', function(){
+    gulp.src('style.css')
+    .pipe(uglifyCss())
+    .pipe(gulp.dest('dist/css'));
+});
